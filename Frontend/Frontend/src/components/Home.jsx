56e870712f4b0caf068e27670d53ed05cda7bbb1
Sayan Das <sayandasraja56@gmail.com>
2024-08-14 10:35:35 +0530

    add leftsidebar

--- Frontend/Frontend/src/components/Home.jsx
+++ Frontend/Frontend/src/components/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import RightSidebar from './RightSidebar'
+import Feed from './Feed'
 
 function Home() {
   return (
-    <div>
-        Home
-      
+    <div className='flex'>
+        <div className='flex-grow'>
+            <Feed />
+            <Outlet />
+        </div>
+        <RightSidebar />
     </div>
-  )
+)
 }
 
 export default Home
